d533420a580773449495527cf3d86226cc274c54
Murilo C Dias <murilocristovaodias@gmail.com>
2024-12-29 12:30:25 -0300

    Criação das camadas chess e boardgame e primeira versão do tabuleiro

---
+++ src/application/UI.java
@@ -0,0 +1,31 @@
+package application;
+
+import chess.ChessPiece;
+
+public class UI {
+
+    public static void printBoard(ChessPiece[][] pieces) {
+        System.out.println("    A   B   C   D   E   F   G   H");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
+
+        for (int i=0; i<pieces.length; i++) {
+            System.out.print((8 - i) + " | ");
+            for (int j=0; j< pieces.length; j++) {
+                printPiece(pieces[i][j]);
+                System.out.print(" | ");
+            }
+            System.out.println(8 - i);
+            System.out.println("  +---+---+---+---+---+---+---+---+");
+        }
+        System.out.println("    A   B   C   D   E   F   G   H");
+    }
+
+    private static void printPiece(ChessPiece piece) {
+        if (piece == null) {
+            System.out.print(" ");
+        }
+        else {
+            System.out.println(piece);
+        }
+    }
+}
